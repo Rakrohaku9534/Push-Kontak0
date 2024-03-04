b9fdd21b6751f072bfdaa3c8c047430220892b84
Fahrul26 <rakrohaku@gmail.com>
2024-03-04 16:47:49 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-global.owner = ["62887435047326"]
+global.owner = ["6281952063349"]
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
-global.pairingNumber = ""
+global.pairingNumber = "6281952063349"
